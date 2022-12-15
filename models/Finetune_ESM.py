48a4b546bb207a96b37553d4531cb2b14249a10c
jasperhyp <jasperhyp@gmail.com>
2022-12-14 22:18:28 -0500

    finetune_esm class fix

--- models/Finetune_ESM.py
+++ models/Finetune_ESM.py
@@ -5,22 +5,23 @@ from esm import pretrained
 
 class FinetuneESM(nn.Module):
     def __init__(self, esm_model_name, first_finetune_layer=None):
+        super().__init__()
         
         # handle fine-tuning
-        self.esm_emb, self.alphabet = pretrained.load_model_and_alphabet_hub(esm_model_name)
+        self.esm_embed, self.alphabet = pretrained.load_model_and_alphabet_hub(esm_model_name)
         self.num_layers = self.esm_embed.num_layers
+        
+        assert first_finetune_layer is not None, "must provide the first layer to finetune"
         self.first_finetune_layer = first_finetune_layer if first_finetune_layer!=-1 else self.num_layers-1
         
-        if first_finetune_layer is not None and first_finetune_layer != 0:
-            finetune_layers = set(range(first_finetune_layer, self.num_layers))
+        if self.first_finetune_layer is not None:
+            finetune_layers = set([str(layer) for layer in range(self.first_finetune_layer, self.num_layers)])
         
         for k, v in self.esm_embed.named_parameters():
             if k.split('.')[1] not in finetune_layers:
                 v.requires_grad = False
-                print(f"Freeze {k}")
             elif 'embed' in k:  # always freeze embedding layer
                 v.requires_grad = False
-                print(f"Freeze {k}")
             else:
                 print(f"Not freeze {k}")
 
@@ -28,7 +29,7 @@ class FinetuneESM(nn.Module):
         residue_embeddings = {}
         
         for gene, toks in batch_dict['esm-tokens'].items():
-            results = self.esm_emb(toks, repr_layers=[self.num_layers])  #  {gene_name:[batch, gene_len+2, vocab_size]}
+            results = self.esm_embed(toks, repr_layers=[self.num_layers])  #  {gene_name:[batch, gene_len+2, vocab_size]}
             residue_embeddings[gene] = results["representations"][self.num_layers]
         
         return residue_embeddings  # {gene_name:[batch, gene_len+2, feat_dim]}
